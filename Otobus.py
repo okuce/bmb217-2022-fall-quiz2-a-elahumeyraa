d40cbc911f29e8550623036e562cdbfd7b0cdbf6
elahumeyraa <114661660+elahumeyraa@users.noreply.github.com>
2022-10-06 22:26:19 +0300

    Update Otobus.py

--- Otobus.py
+++ Otobus.py
@@ -4,29 +4,37 @@ Ela Hümeyra Hacıçavuşoğlu 2020717050 A grubu
 
 class Otobus:
     """Otobus bilet satis takip sinifi"""
-    plaka=""
-    nerden=""
-    nereye=""
-    _koltuk_sayisi=0
+    plaka:str=""
+    nerden:str=""
+    nereye:str=""
+    koltuk_sayisi:int=0
+    _dolu_koltuk_sayisi:int=0
 
-    def __init__(self,plaka,nerden,nereye,_koltuk_sayisi):
+
+    def __init__(self,plaka,nerden,nereye,koltuk_sayisi) -> None:
+        """Constructor method
+
+        Args:
+            plaka otobüs plakası
+            nerden güzargah başlangıç noktası
+            nereye güzargah bitiş noktası
+            koltuk sayısı otobüste yer alan koltuk sayısı
+        """
         self.plaka=plaka
         self.nerden=nerden
         self.nereye=nereye
-        self._koltuk_sayisi=_koltuk_sayisi
+        self.koltuk_sayisi=koltuk_sayisi
     
     def bilet_sat(self):
         """Otobusteki dolu koltuk sayisini 1 artirir"""
-        satislar = self._koltuk_sayisi-1
-        dolu_koltuk=satislar
-        return dolu_koltuk
+        if self._dolu_koltuk_sayisi<self.koltuk_sayisi:
+            self._dolu_koltuk_sayisi += 1
 
     def bilet_iade(self):
         """Otobusteki dolu koltuk sayisini 1 azaltir"""
-        iadeler= self._koltuk_sayisi + 1
-        bos_koltuk=iadeler
-        return bos_koltuk
+        if self._dolu_koltuk_sayisi>0:
+            self._dolu_koltuk_sayisi -= 1
 
     def durum_yaz(self):
         """Otobusun guzergahini, plakasini,bos ve dolu koltuk sayisini yazdirir"""
-        print("{}, {}, {}, {}, {}".format(self.nerden,self.nereye,self.plaka,self.bilet_iade(),self.bilet_sat()))
+        print("{},{},{},{},{}".format(self.nerden,self.nereye,self.plaka,self.koltuk_sayisi,self._dolu_koltuk_sayisi))
