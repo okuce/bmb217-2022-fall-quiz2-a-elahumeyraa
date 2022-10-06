63bf41e9c7f00bd65b64602fec437729626c09b5
elahumeyraa <114661660+elahumeyraa@users.noreply.github.com>
2022-10-06 14:35:44 +0300

    Update Otobus.py

--- Otobus.py
+++ Otobus.py
@@ -1,18 +1,32 @@
+"""
+Ela Hümeyra Hacıçavuşoğlu 2020717050 A grubu
+"""
+
 class Otobus:
     """Otobus bilet satis takip sinifi"""
-    
+    plaka=""
+    nerden=""
+    nereye=""
+    _koltuk_sayisi=0
 
+    def __init__(self,plaka,nerden,nereye,_koltuk_sayisi):
+        self.plaka=plaka
+        self.nerden=nerden
+        self.nereye=nereye
+        self._koltuk_sayisi=_koltuk_sayisi
+    
     def bilet_sat(self):
         """Otobusteki dolu koltuk sayisini 1 artirir"""
-        
-    
+        satislar = self._koltuk_sayisi-1
+        dolu_koltuk=satislar
+        return dolu_koltuk
+
     def bilet_iade(self):
         """Otobusteki dolu koltuk sayisini 1 azaltir"""
-        
+        iadeler= self._koltuk_sayisi + 1
+        bos_koltuk=iadeler
+        return bos_koltuk
 
-    
     def durum_yaz(self):
         """Otobusun guzergahini, plakasini,bos ve dolu koltuk sayisini yazdirir"""
-        
-
-
+        print("{}, {}, {}, {}, {}".format(self.nerden,self.nereye,self.plaka,self.bilet_iade(),self.bilet_sat()))
